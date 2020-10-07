5ebbc37d4c3eb394e1c01512382e10d57e937f24
mgwalsh <markusgwalsh@gmail.com>
2020-10-07 15:37:56 +0300

    Mixed model analyses of national level cereal-yield gap data

---
+++ cereal_yield_gaps.R
@@ -0,0 +1,34 @@
+# Mixed model analyses of national level cereal-yield gap data
+# M.G. Walsh, October 2020
+
+# Required packages
+# install.packages(c("downloader","arm")), dependencies=TRUE)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(arm)
+  require(dplyr)
+})
+
+# Data setup --------------------------------------------------------------
+# Create a data folder in your current working directory
+dir.create("Yield_gap", showWarnings=F)
+setwd("./Yield_gap")
+
+# download data
+download("", "Cereal_yield_gap.zip", mode = "wb")
+unzip("Cereal_yield_gap.zip", overwrite = T)
+cereal_panel <- read.table("cereal_panel.csv", header = T, sep = ",")
+hdi <- read.table("hdi.csv", header = T, sep = ",")
+arable_perc <- read.table("arable_perc.csv", header = T, sep = ",")
+ag_employ <- read.table("ag_employ.csv", header = T, sep = ",")
+area <- read.table("maize_rice_wheat_area.csv", header = T, sep = ",")
+cereal_prod <- read.table("cereal_prod.csv", header = T, sep = ",")
+maize_prod <- read.table("maize_prod.csv", header = T, sep = ",")
+rice_prod <- read.table("rice_prod.csv", header = T, sep = ",")
+wheat_prod <- read.table("wheat_prod.csv", header = T, sep = ",")
+fert_consumption <- read.table("fert_consumption.csv", header = T, sep = ",")
+
+# Cereal yield calculation
+cepan <- merge(cereal_panel, cereal_prod, by="id")
+cepan <- cepan[complete.cases(cepan[ ,7:8]),]
+cepan$cereal_yield <- cepan$cereal_prod / cepan$cereal_area ## Mg / ha
