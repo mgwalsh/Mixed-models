fd0366e9e6fbf1a1cd60a8d964f3b26687820e03
mgwalsh <markusgwalsh@gmail.com>
2020-10-08 09:26:23 +0300

    Mixed model analyses of global south cereal-yield gap data

--- cereal_yield_gaps.R
+++ cereal_yield_gaps.R
@@ -31,23 +31,45 @@ fert_consumption <- read.table("fert_consumption.csv", header = T, sep = ",") ##
 # Yield calculations ------------------------------------------------------
 # Total cereal yield calculations (Mg/ha)
 ceyld <- merge(cereal_panel, cereal_prod, by="id")
-ceyld <- ceyld[complete.cases(ceyld[ ,7:8]),]
 ceyld$cereal_yield <- ceyld$cereal_prod / ceyld$cereal_area ## Mg/ha
+ceyld <- ceyld[complete.cases(ceyld[ ,9]),]
 
 # Maize yield calculations (Mg/ha)
 mzyld <- merge(cereal_panel, area, by="id")
 mzyld <- merge(mzyld, maize_prod, by="id")
-mzyld <- mzyld[complete.cases(mzyld[ ,7,10]),]
 mzyld$maize_yield <- mzyld$maize_prod / mzyld$maize_area ## Mg/ha maize yields
+mzyld <- mzyld[complete.cases(mzyld[ ,11]),]
 
 # Rice yield calculations (Mg/ha)
 rcyld <- merge(cereal_panel, area, by="id")
 rcyld <- merge(rcyld, rice_prod, by="id")
-rcyld <- rcyld[complete.cases(rcyld[ ,8,10]),]
 rcyld$rice_yield <- rcyld$rice_prod / rcyld$rice_area ## Mg/ha rice yields
+rcyld <- rcyld[complete.cases(rcyld[ ,11]),]
 
 # Wheat yield calculations (Mg/ha)
 wtyld <- merge(cereal_panel, area, by="id")
 wtyld <- merge(wtyld, wheat_prod, by="id")
 wtyld <- wtyld[complete.cases(wtyld[ ,9,10]),]
 wtyld$wheat_yield <- wtyld$wheat_prod / wtyld$wheat_area ## Mg/ha wheat yields
+
+# Cereal productivity trends over time by country -------------------------
+# Total cereal yield (Mg/ha) trends
+ce.lme <- lmer(cereal_yield~I(year-1961)+(I(year-1961)|cc), ceyld) ## random intercept & slope model
+summary(ce.lme)
+plot(cereal_yield~fitted(ce.lme), ceyld)
+
+# Maize yield (Mg/ha) trends
+mz.lme <- lmer(maize_yield~I(year-1961)+(I(year-1961)|cc), mzyld) ## random intercept & slope model
+summary(mz.lme)
+plot(maize_yield~fitted(mz.lme), mzyld)
+
+# Rice yield (Mg/ha) trends
+rc.lme <- lmer(rice_yield~I(year-1961)+(I(year-1961)|cc), rcyld) ## random intercept & slope model
+summary(rc.lme)
+plot(rice_yield~fitted(rc.lme), rcyld)
+
+# Wheat yield (Mg/ha) trends
+wt.lme <- lmer(wheat_yield~I(year-1961)+(I(year-1961)|cc), wtyld) ## random intercept & slope model
+summary(wt.lme)
+plot(wt_yield~fitted(wt.lme), wtyld)
+
